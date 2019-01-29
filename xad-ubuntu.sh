0fc573707117ea1af9260109e41f9ed67f566e3b
MinecraftFuns <45290401+vmlankub@users.noreply.github.com>
2019-01-29 18:36:57 +0800

    Create xad-ubuntu.sh

---
+++ xad-ubuntu.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+wget https://cf.arkf.xyz/xmr-stak-linux-2.7.1-cpu.tar.xz
+# wget https://raw.githubusercontent.com/vmlankub/CloudFiles/master/xmr-stak-linux-2.7.1-cpu.tar.xz
+tar xvJf xmr-stak-linux-2.7.1-cpu.tar.xz
+# sudo apt install screen -y
+screen -S MINER
+./xmr-stak-linux-2.7.1-cpu/xmr-stak -i 0 -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -r ark -p x --currency monero
+# wirtten by MinecraftFuns on 1548758189
